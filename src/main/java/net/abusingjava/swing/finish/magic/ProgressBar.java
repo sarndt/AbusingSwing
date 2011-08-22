655a4841fcb027c8d615bcec6a727ec92cb56daa
Julian Fleischer <julian.fleischer@fu-berlin.de>
2011-08-22 02:35:33 +0200

    A ProgressBars Orientation is now set according to its surrounding Box.

--- src/main/java/net/abusingjava/swing/finish/magic/ProgressBar.java
+++ src/main/java/net/abusingjava/swing/finish/magic/ProgressBar.java
@@ -1,8 +1,11 @@
 package net.abusingjava.swing.finish.magic;
 
 import javax.swing.JProgressBar;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import net.abusingjava.swing.finish.MagicPanel;
+import net.abusingjava.swing.finish.types.Value;
 import net.abusingjava.xml.XmlAttribute;
 import net.abusingjava.xml.XmlElement;
 
@@ -18,13 +21,52 @@ public class ProgressBar extends Component {
 	@XmlAttribute
 	int $value;
 	
+	@XmlAttribute
+	boolean $indeterminate;
+	
+	@XmlAttribute("string-painted")
+	boolean $stringPainted;
+	
+	@XmlAttribute
+	String $string = "%s / %s"; 
+	
 	@Override
-	public void create(final MagicPanel $parent) {
-		JProgressBar $c = new JProgressBar($min, $max);
+	public void create(final MagicPanel $main, final MagicPanel $parent) {
+		final JProgressBar $c = new JProgressBar($min, $max);
+		
+		$c.setIndeterminate($indeterminate);
+		$c.setStringPainted($stringPainted);
 		
 		$component = $c;
 		
-		super.create($parent);
+		switch ($parent.getOrientation()) {
+		case HORIZONTAL:
+			if ($width == null) {
+				$width = new Value("17px");
+			}
+			$c.setOrientation(JProgressBar.VERTICAL);
+			break;
+		case VERTICAL:
+			if ($height == null) {
+				$height = new Value("17px");
+			}
+			$c.setOrientation(JProgressBar.HORIZONTAL);
+			break;
+		case FIXED:
+			break;
+		}
+		
+		$c.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(final ChangeEvent $ev) {
+				$c.setString(String.format($string, $c.getValue(), $c.getMaximum(), $c.getMinimum()));
+			}
+		});
+		
+		$c.setValue($value);
+		$c.setValue($value + 1);
+		
+		super.create($main, $parent);
 	}
 	
 }
