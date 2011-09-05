fc29698dd322a6244919ff3b6de33454d325fc9a
Julian Fleischer <julian.fleischer@fu-berlin.de>
2011-09-05 23:33:30 +0200

    setDateFormat implemented.

--- src/main/java/net/abusingjava/swing/magix/XTable.java
+++ src/main/java/net/abusingjava/swing/magix/XTable.java
@@ -9,37 +9,18 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
-import org.jdesktop.swingx.JXTable;
-
 import net.abusingjava.swing.AbusingSwing;
+
+import org.jdesktop.swingx.JXTable;
 
 
 public class XTable extends JXTable {
 
 	private static final long serialVersionUID = -1110662927738700425L;
-	private final SimpleDateFormat $dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private SimpleDateFormat $dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	public XTable() {
-		/*
-		setDefaultRenderer(java.util.Date.class, new DefaultTableCellRenderer() {
-
-			private static final long serialVersionUID = 8519130125457693769L;
-
-			@Override
-			public java.awt.Component getTableCellRendererComponent(final JTable $table,
-					final Object $value, final boolean $isSelected, final boolean $hasFocus, final int $row, final int $column) {
-				
-				super.getTableCellRendererComponent($table, $value, $isSelected, $hasFocus, $row, $column);
-				if ($value instanceof Date) {
-					String $date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format((Date) $value);
-					this.setText($date);
-				}
-				
-				return this;
-			}
-
-		});
-		*/
+		
 	}
 	
 	public XTable(final String[] $columns) {
@@ -56,7 +37,7 @@ public class XTable extends JXTable {
 	}
 	
 	public void setDateFormat(final String $dateFormat) {
-		
+		this.$dateFormat = new SimpleDateFormat($dateFormat);
 	}
 	
 	public String getDateFormat() {
