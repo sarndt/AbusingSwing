e96dd92d435879682a2daf2b5dca57e88ed98a55
Julian Fleischer <julian.fleischer@fu-berlin.de>
2011-10-09 13:55:32 +0200

    Support for values in List

--- src/main/java/net/abusingjava/swing/magic/List.java
+++ src/main/java/net/abusingjava/swing/magic/List.java
@@ -7,9 +7,14 @@ import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import net.abusingjava.AbusingArrays;
+import net.abusingjava.functions.AbusingFunctions;
 import net.abusingjava.swing.MagicPanel;
+import net.abusingjava.swing.magic.ComboBox.Val;
+import net.abusingjava.swing.magix.types.JavaType;
 import net.abusingjava.swing.magix.types.MethodType;
 import net.abusingjava.xml.XmlAttribute;
+import net.abusingjava.xml.XmlChildElements;
 import net.abusingjava.xml.XmlElement;
 
 @XmlElement("list")
@@ -17,13 +22,39 @@ public class List extends Component {
 
 	@XmlAttribute
 	MethodType $onselect;
-	
+
+	@XmlChildElements
+	Val[] $values;
+
+	@XmlAttribute("from")
+	JavaType $from;
+
 	@Override
 	public void create(final MagicPanel $main, final MagicPanel $parent) {
+		 
+		if ($from != null) {
+			Object[] $v = new Object[] {};
+			if ($from.getJavaType().isEnum()) {
+				$v = (Object[]) AbusingFunctions.callback($from.getJavaType(), "values").call();
+			} else if (($from != null) && Iterable.class.isAssignableFrom($from.getJavaType())) {
+				try {
+					$v = AbusingArrays.toArray((Iterable<?>) $from.getJavaType().newInstance());
+				} catch (Exception $exc) {
+				}
+			}
+			Val[] $values = new Val[$v.length];
+			for (int $i = 0; $i < $v.length; $i++) {
+				$values[$i] = new Val($v[$i].toString());
+			}
+			this.$values = AbusingArrays.concat(this.$values, $values);
+		}
 		final DefaultListModel $m = new DefaultListModel();
+		if ($values != null) {
+			for (Object $value : $values) {
+				$m.addElement($value);
+			}
+		}
 		final JList $c = new JList($m);
-		
-		
 		
 		$c.setVisibleRowCount(-1);
 		$c.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
