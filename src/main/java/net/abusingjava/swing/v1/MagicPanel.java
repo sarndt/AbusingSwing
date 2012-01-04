26163d6b0fe1c0cbee5d82840dd0709ce285ab82
Julian Fleischer <julian.fleischer@fu-berlin.de>
2012-01-04 16:52:29 +0100

    Adapted to changes in AbusingJava

--- src/main/java/net/abusingjava/swing/v1/MagicPanel.java
+++ src/main/java/net/abusingjava/swing/v1/MagicPanel.java
@@ -4,13 +4,11 @@ import java.io.InputStream;
 
 import net.abusingjava.Author;
 import net.abusingjava.Since;
-import net.abusingjava.Version;
 import net.abusingjava.swing.v1.components.XPanelDefinition;
 import net.abusingjava.xml.AbusingXML;
 
 @Author("Julian Fleischer")
-@Version(value = "1.0", date = "2011-10-23")
-@Since(version = "1.0", value = "2011-10-15")
+@Since("2011-10-15")
 public class MagicPanel {
 
 	@SuppressWarnings("unused")
